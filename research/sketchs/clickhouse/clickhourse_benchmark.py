696098c5e3e7e2036fbba12f730ac04b924cf9d6
Admin <apenshin@gmail.com>
2022-08-25 12:36:48 +0300

    Test

--- research/sketchs/clickhouse/clickhourse_benchmark.py
+++ research/sketchs/clickhouse/clickhourse_benchmark.py
@@ -4,8 +4,6 @@ from datetime import datetime
 
 from clickhouse_driver import Client
 
-
-
 MESSAGES_BATCH_SIZE = 1000000
 BATCHES = 100
 
@@ -20,14 +18,12 @@ movie_lengths = [randrange(MOVIE_MAX_LEN) for i in range(1, MOVIE_COUNT)]
 
 client = Client(host='localhost')
 
-
 time_now = datetime.now()
 time_now = time_now.strftime("%Y-%m-%d %H:%M:%S")
 
-
 for i in range(1, BATCHES):
     for user_id in users:
-        user_current_movie_id = movies[randrange(MOVIE_COUNT-1)]
+        user_current_movie_id = movies[randrange(MOVIE_COUNT - 1)]
         for tick in movie_lengths:
             query = f"INSERT INTO movies_statistics.view_stat (movie_id, user_id, eventTime, view_run_time) " \
                     f"VALUES ('{user_current_movie_id}', '{user_id}', '{time_now}', {tick})"
