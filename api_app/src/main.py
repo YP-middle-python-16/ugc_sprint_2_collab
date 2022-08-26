82920fdc97a4405ee5e894413cf76456c82d5adb
arthur hodosh <arthurhodosh2011@gmail.com>
2022-08-26 18:08:48 +0300

    add sentry module

--- api_app/src/main.py
+++ api_app/src/main.py
@@ -1,13 +1,28 @@
 import aiokafka
+import sentry_sdk
 import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.logger import logger
 from fastapi.responses import ORJSONResponse
+from sentry_sdk.integrations.fastapi import FastApiIntegration
+from sentry_sdk.integrations.starlette import StarletteIntegration
 
 from api.v1 import events
 from core.config import settings
 from db import kafka
 from utils import backoff
+
+sentry_sdk.init(
+    integrations=[
+        StarletteIntegration(),
+        FastApiIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production,
+    traces_sample_rate=1.0,
+)
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
