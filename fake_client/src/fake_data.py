4e18d44418e557a2f6462965422bd0cc96c7a412
arthur hodosh <arthurhodosh2011@gmail.com>
2022-09-05 23:21:20 +0300

    add fake data

--- fake_client/src/fake_data.py
+++ fake_client/src/fake_data.py
@@ -11,3 +11,7 @@ MOVIES = [uuid.uuid4() for _ in range(1, MOVIE_COUNT)]
 MOVIE_LENGTH = [randrange(MOVIE_MAX_LEN) for _ in range(1, MOVIE_COUNT)]
 
 LIKE_MAX_LEN = 10
+SORT_ORDER_MAX_LEN = 10
+
+CATEGORIES = ['comedy', 'drama', 'thriller', 'sci-fi', 'documentary', 'action', 'fantasy']
+CATEGORIES_LENGTH = len(CATEGORIES)
