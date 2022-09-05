49d263eff2db08fd5d112bb7440d1565b52fbb4e
arthur hodosh <arthurhodosh2011@gmail.com>
2022-09-05 21:48:32 +0300

    add models

--- fake_client/src/models.py
+++ fake_client/src/models.py
@@ -25,3 +25,27 @@ class FilmViewEvent(ORJSONModel):
 class EventMessage(ORJSONModel):
     key: str
     value: str
+
+
+class LikeEvent(ORJSONModel):
+    user_id: str
+    movie_id: str
+    event_time: datetime
+    score: int
+
+
+class Comment(ORJSONModel):
+    user_id: str = None
+    movie_id: str = None
+    event_time: datetime = None
+    title: str = None
+    body: str = None
+    score: int = None
+
+
+class Bookmark(ORJSONModel):
+    user_id: str = None
+    movie_id: str = None
+    label: str = None
+    category: str = None
+    sort_order: int = None
