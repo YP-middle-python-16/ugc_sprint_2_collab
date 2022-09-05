d865d4e2fb4e5c237752c8e725881a668be755a8
arthur hodosh <arthurhodosh2011@gmail.com>
2022-09-05 21:59:39 +0300

    remove unused

--- fake_client/src/fake_data.py
+++ fake_client/src/fake_data.py
@@ -10,9 +10,4 @@ MOVIES = [uuid.uuid4() for _ in range(1, MOVIE_COUNT)]
 
 MOVIE_LENGTH = [randrange(MOVIE_MAX_LEN) for _ in range(1, MOVIE_COUNT)]
 
-####################################
-
-LIKE_COUNT = 200
-MOVIE_COUNT = 300
 LIKE_MAX_LEN = 10
-
