0bbacc526a1304400bcc46ed9f736bb2c158f6ef
arthur hodosh <arthurhodosh2011@gmail.com>
2022-09-05 21:48:14 +0300

    add like data

--- fake_client/src/fake_data.py
+++ fake_client/src/fake_data.py
@@ -9,3 +9,10 @@ USERS = [uuid.uuid4() for _ in range(1, USER_COUNT)]
 MOVIES = [uuid.uuid4() for _ in range(1, MOVIE_COUNT)]
 
 MOVIE_LENGTH = [randrange(MOVIE_MAX_LEN) for _ in range(1, MOVIE_COUNT)]
+
+####################################
+
+LIKE_COUNT = 200
+MOVIE_COUNT = 300
+LIKE_MAX_LEN = 10
+
